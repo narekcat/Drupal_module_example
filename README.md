eeb2bb00dbc3e1aa3f3fee37cd82710a26683d4e
Narek Vardzelyan <narek.vardzelyan@of.am>
2015-12-09 17:57:12 +0400

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Drupal_module_example
---
+++ src/Plugin/Block/HelloBlock.php
@@ -0,0 +1,46 @@
+<?php
+namespace Drupal\hello_world\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Block\BlockPluginInterface;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a 'Hello' Block
+ *
+ * @Block(
+ * id = "hello_block",
+ * admin_label = @Translation("Hello block"),
+ * )
+ */
+class HelloBlock extends BlockBase implements BlockPluginInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return array(
+      '#markup' => $this->t('Hello, World!'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+
+    $config = $this->getConfiguration();
+
+    $form['hello_block_settings'] = array (
+      '#type' => 'textfield',
+      '#title' => $this->t('Who'),
+      '#description' => $this->t('Who do you want to say hello to?'),
+      '#default_value' => isset($config['hello_block_settings']) ? $config['hello_block_settings'] : ''
+    );
+
+    return $form;
+  }
+}
+
+?>
